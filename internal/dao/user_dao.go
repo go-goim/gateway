5d8d8b10e96c2ab0056502b0b6f92b283829dc9a
yushan <shan.yu@shopee.com>
2022-06-01 20:35:56 +0800

    fix: separate from core(goim)

---
+++ internal/dao/user_dao.go
@@ -0,0 +1,45 @@
+package dao
+
+import (
+	"context"
+	"sync"
+
+	redisv8 "github.com/go-redis/redis/v8"
+
+	"github.com/go-goim/core/pkg/consts"
+
+	"github.com/go-goim/gateway/internal/app"
+)
+
+var (
+	userDao     *UserDao
+	userDaoOnce sync.Once
+)
+
+type UserDao struct {
+	rdb *redisv8.Client
+	// mysql.DB get from context, because we may need use transaction
+}
+
+func GetUserDao() *UserDao {
+	userDaoOnce.Do(func() {
+		userDao = &UserDao{
+			rdb: app.GetApplication().Redis,
+		}
+	})
+	return userDao
+}
+
+// GetUserOnlineAgent get user online agent from redis
+func (u *UserDao) GetUserOnlineAgent(ctx context.Context, uid string) (string, error) {
+	key := consts.GetUserOnlineAgentKey(uid)
+	val, err := u.rdb.Get(ctx, key).Result()
+	if err != nil {
+		if err == redisv8.Nil {
+			return "", nil
+		}
+		return "", err
+	}
+
+	return val, nil
+}
---
+++ internal/service/discover_push_server.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strings"
+
+	"github.com/go-goim/core/pkg/registry"
+
+	"github.com/go-goim/gateway/internal/app"
+)
+
+func LoadMatchedPushServer(ctx context.Context) (string, error) {
+	list, err := registry.GetService(ctx, app.GetApplication().Config.SrvConfig.PushService)
+	if err != nil {
+		return "", err
+	}
+
+	if len(list) == 0 {
+		return "", fmt.Errorf("service not found")
+	}
+
+	var eps []string
+	for _, instance := range list {
+		for _, ep := range instance.Endpoints {
+			if strings.HasPrefix(ep, "http") {
+				eps = append(eps, ep)
+			}
+		}
+	}
+	if len(eps) == 0 {
+		return "", fmt.Errorf("no matched service")
+	}
+
+	return eps[rand.Int()%len(eps)], nil // nolint:gosec
+}
