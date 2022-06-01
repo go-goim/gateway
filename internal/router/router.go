e872b970cb4684f0d89f5967553825c3a12a862c
yusank <yusankurban@gmail.com>
2022-06-01 20:35:56 +0800

    fix: separate from core(goim)

---
+++ internal/router/router.go
@@ -0,0 +1,30 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-goim/core/pkg/router"
+
+	v1 "github.com/go-goim/gateway/internal/router/v1"
+)
+
+type rootRouter struct {
+	router.Router
+}
+
+func newRootRouter() *rootRouter {
+	r := &rootRouter{
+		Router: &router.BaseRouter{},
+	}
+
+	r.init()
+	return r
+}
+
+func (r *rootRouter) init() {
+	r.Register("/v1", v1.NewRouter())
+}
+
+func RegisterRouter(g *gin.RouterGroup) {
+	r := newRootRouter()
+	r.Load(g)
+}
